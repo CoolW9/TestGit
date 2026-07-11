4282448deb24a9de9d8b0729ae4e8cf51f54a2f3
CoolW9 <w18502657325@163.com>
2026-07-11 23:19:04 +0800

    test02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 900; i++) {
+        for (int i = 1; i <= 300; i++) {
             System.out.println(i);
         }
         System.out.println(1111111111);
         System.out.println(222222222);
+        System.out.println(3333);
     }
 }
