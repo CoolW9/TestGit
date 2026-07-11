758e4d52b6bef5889171232147d35784656b6fe2
CoolW9 <w18502657325@163.com>
2026-07-11 23:15:17 +0800

    test02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 800; i++) {
+        for (int i = 1; i <= 900; i++) {
             System.out.println(i);
         }
     }
